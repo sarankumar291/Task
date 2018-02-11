78b3aa47e2d2541995240b1c9372aab8274460fb
sarankumar <atrsarankumar@gmail.com>
2018-02-11 20:22:30 +0530

    Added UI, API & refresh controller added

--- Task/Controllers/ViewController.swift
+++ Task/Controllers/ViewController.swift
@@ -9,17 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var activityDataModel: ActivityDataModel? = ActivityDataModel()
+    internal var tableView: UITableView!
+    
+    // MARK: Refresh control
+    private lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = UIColor.Theme
+        refreshControl.addTarget(self, action: #selector(loadActivities), for: .valueChanged)
+        return refreshControl
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setupTableView()
+        self.setupView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setupView() {
+        self.view.backgroundColor = UIColor.white
+        self.showLoader()
+        activityDataModel?.getActivityList(completed: {
+            self.hideLoader()
+            DispatchQueue.main.async {
+                self.title = self.activityDataModel?.title
+                self.tableView.reloadData()
+            }
+        })
     }
-
-
+    
+    private func  setupTableView() {
+        tableView = UITableView()
+        tableView.register(ActivityTableVIewCell.self, forCellReuseIdentifier: "cell")// register cell name
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
+        self.tableView.addSubview(refreshControl)
+        tableView.separatorStyle = .none
+        let safeView = view.safeAreaLayoutGuide
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        let leadingConstraint = tableView.leadingAnchor.constraint(equalTo: safeView.leadingAnchor)
+        let trailingConstraint = tableView.trailingAnchor.constraint(equalTo: safeView.trailingAnchor)
+        let topConstraint = tableView.topAnchor.constraint(equalTo: safeView.topAnchor)
+        let bottomConstraint = tableView.bottomAnchor.constraint(equalTo: safeView.bottomAnchor)
+        view.addConstraints([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
+    }
+    
+    @objc func loadActivities() {
+        activityDataModel?.getActivityList(completed: {
+            self.hideLoader()
+            DispatchQueue.main.async {
+                if self.refreshControl.isRefreshing {
+                    self.refreshControl.endRefreshing()
+                }
+                self.title = self.activityDataModel?.title
+                self.tableView.reloadData()
+            }
+        })
+    }
+    
+    deinit {
+        print("ViewController Deinit")
+        activityDataModel = nil
+    }
 }
 
