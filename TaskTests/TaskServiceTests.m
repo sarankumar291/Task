6f7bca73afac814dd4cf490645881cfa7f3621e9
sarankumar <atrsarankumar@gmail.com>
2018-02-13 20:20:37 +0530

    Test cases are written

--- TaskTests/TaskServiceTests.m
+++ TaskTests/TaskServiceTests.m
@@ -7,9 +7,10 @@
 //
 
 #import <XCTest/XCTest.h>
+// Extensions
+#import "WebServiceURLs.h"
 
 @interface TaskServiceTests : XCTestCase
-
 @end
 
 @implementation TaskServiceTests
@@ -34,6 +35,27 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+- (void)testWebService {
+    // Initialize necessary objects
+    NSURLSession *session = [NSURLSession sharedSession];
+    // Execute test
+    XCTestExpectation *expectation = [self expectationWithDescription:@"API call async"];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString: baseURL] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        XCTAssertNil(error, @"dataTaskWithURL error %@", error);
+        if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+            NSInteger statusCode = [(NSHTTPURLResponse *) response statusCode];
+            XCTAssertEqual(statusCode, 200, @"status code was not 200; was %ld", (long)statusCode);
+        }
+        XCTAssert(data, @"data nil");
+        // do additional tests on the contents of the `data` object here, if you want
+        // when all done, Fulfill the expectation
+        [expectation fulfill];
+        
+    }];
+    [dataTask resume];
+    [self waitForExpectationsWithTimeout:10.0 handler:nil];
 }
 
 @end
--- TaskTests/ViewControllerTests.m
+++ TaskTests/ViewControllerTests.m
@@ -8,20 +8,27 @@
 
 #import <XCTest/XCTest.h>
 
-@interface ViewControllerTests : XCTestCase
+// Views
+#import "ViewController.h"
 
+@interface ViewControllerTests : XCTestCase
+@property (nonatomic, strong) ViewController * viewController;
 @end
 
 @implementation ViewControllerTests
 
 - (void)setUp {
     [super setUp];
+    
+    _viewController = [[ViewController alloc] init];
+    _viewController.tableView = [[UITableView alloc] init];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    _viewController = nil;
 }
 
 - (void)testExample {
@@ -34,6 +41,24 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+- (void)testOutlets {
+    XCTAssertTrue(_viewController.tableView, @"TableView not initiated");
+}
+
+- (void)testRegisteredNibForTableViewCell {
+    [_viewController.tableView registerClass:[ActivityTableViewCell class] forCellReuseIdentifier:@"cell"];
+    ActivityTableViewCell *cell = [_viewController.tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath: [NSIndexPath indexPathForRow:0 inSection:0]];
+    XCTAssertTrue(cell,@"Cell should be an instance of ActivityTableViewCell");
+}
+
+- (void)testLoadTableView {
+    [_viewController.tableView reloadData];
+}
+
+- (void)testTableViewDatasourceSetup {
+    XCTAssertEqual(_viewController.tableView.numberOfSections, 1, @"TableView should have one section");
 }
 
 @end
