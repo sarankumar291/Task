c6ccbfb31e6010100835a9706f259e90adf7462a
sarankumar <atrsarankumar@gmail.com>
2018-02-13 20:18:46 +0530

    UIColor Extension added

--- Task/Extensions/UIColor+Extension.m
+++ Task/Extensions/UIColor+Extension.m
@@ -9,5 +9,10 @@
 #import "UIColor+Extension.h"
 
 @implementation UIColor (Extension)
-
++ (UIColor *)theme {
+    return [UIColor colorWithRed: 248.0/255.0 green: 208.0/255.0 blue: 0.0/255.0 alpha: 1.0];
+}
++ (UIColor *)navBarTint {
+    return [UIColor whiteColor];
+}
 @end
