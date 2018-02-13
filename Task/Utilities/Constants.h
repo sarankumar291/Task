d9c739f65d712e99bbc29c1d556a579a3134253a
sarankumar <atrsarankumar@gmail.com>
2018-02-13 20:17:59 +0530

    Constants are added

--- Task/Utilities/Constants.h
+++ Task/Utilities/Constants.h
@@ -9,5 +9,14 @@
 #ifndef Constants_h
 #define Constants_h
 
-
+// TableView Constants
+static float const leadingConstant = 20;
+static float const trailingConstant = -20;
+static float const topAndBottomConstant = 10;
+static float const imageViewHeightConstant = 100;
+static float const bottomLineHeightConstant = 1;
+static float const bottomLineBottomConstant = -1;
+static float const descriptionLabelFontSize = 14;
+static float const titleLabelFontSize = 16;
+static float const navigationBarFontSize = 17;
 #endif /* Constants_h */
